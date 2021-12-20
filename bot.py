10578d792dc6c7821ebabfd4f9f6551038a2a656
astepanov <57390938+andystep1@users.noreply.github.com>
2021-12-20 16:27:54 +0300

    create mcdonalds finder bot

--- bot.py
+++ bot.py
@@ -6,8 +6,10 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from aiohttp.helpers import TOKEN
+#from aiogram.methods import SendLocation
 
 from config import TOKEN
+from placesapi import find_nearest, get_nearest
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
@@ -16,22 +18,33 @@ dp = Dispatcher(bot)
 async def start(message: types.Message):
     user_name = message.from_user.full_name
     user_id = message.from_user.id
-    print(user_name)
-    
-    await message.reply(f'Привет! {user_name}! Твой user_id = {user_id}!')
+    await message.reply(f'Привет! {user_name}! Нажми на кнопку, чтобы отправить свое местоположение и найти ближайший МакДональдс', reply_markup=get_keyboard())
 
-@dp.message_handler() #эхо
-async def echo(message: types.Message):
-    await message.reply(message.text)
+def get_keyboard():
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    button = types.KeyboardButton("Найти", request_location=True)
+    keyboard.add(button)
+    return keyboard
 
-@dp.message_handler(content_types=['text'])
-async def start(message: types.Message):
-    await message.reply(message.text)
+@dp.message_handler(content_types=['location'])
+async def handle_location(message: types.Message):
+    lat = message.location.latitude
+    lon = message.location.longitude
+    loc = (lat, lon)
+    aaa = get_nearest(lat, lon)
+    reply = find_nearest(loc, aaa)
+    reslat = reply[0]
+    reslon = reply[1]
+    await message.answer('Ближайший Макдональдс находится здесь')
+    await message.answer_location(reslat, reslon, reply_markup=types.ReplyKeyboardRemove())
 
-@dp.message_handler(content_types=['photo'])
-async def photo(message: types.Message):
-    #user_name = message.from_user.full_name
-    chat_id = message.chat.id
-    await bot.send_photo(chat_id, 'https://sun9-74.userapi.com/impg/iEa6AJ2_XN_FbOvEM7yioWaHg2YR3-U-U_E3pw/jh0iclB4ED8.jpg?size=1280x1201&quality=95&sign=e684a11b1faeaa24a33e210339152f16&type=album')
+@dp.message_handler(commands=['locate_me'])
+async def cmd_locate_me(message: types.Message):
+    reply = "Click on the the button below to share your location"
+    await message.answer(reply, reply_markup=get_keyboard(), disable_web_page_preview = False)
+
+
+#http://www.google.com/maps/search/mcdonalds/@55.7124618,37.5911376,13z/data=!3m1!4b1
+
 if __name__ == '__main__': #всегда тру
     executor.start_polling(dp)
